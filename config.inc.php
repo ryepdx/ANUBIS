d707e1f2f2cda0043064afb4ca273a41623aac43
ryepdx <ryepdx@gmail.com>
2013-05-19 15:37:51 +0000

    Moved settings to settings.inc.php and created an example.settings.inc.php file. Did this to expedite pushing and pulling updates from my live install.

--- config.inc.php
+++ config.inc.php
@@ -1,31 +1,9 @@
 <?php
-
-$dbdatabase = "anubis_db";
-
-/* MYSQL specific defines */
-$dbusername = "anubis";
-$dbpassword = "h3rakles";
-$dbhost = "localhost";
-/* End MYSQL specific defines */
-
+include("settings.inc.php");
 $version = "2.3";
 
-/* Set desired database interface to 1, others 0 */
-$db_mysql = "1";
-$db_sqlite = "";
-
-
-/* Sockets time out, set shorter if you have mutiple rigs specified in anubis
-   but turned off / disabled and page refreshes take a long time */
-
-$socket_timeout = 3;
-
-
-
-
-
-
 /*****************************************************************************/
+
 $auto_inc = "";
 $table_props = "";
 
---
+++ example.settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+$dbdatabase = "anubis_db";
+
+/* MYSQL specific defines */
+$dbusername = "anubis";
+$dbpassword = "h3rakles";
+$dbhost = "localhost";
+/* End MYSQL specific defines */
+
+/* Set desired database interface to 1, others 0 */
+$db_mysql = "1";
+$db_sqlite = "";
+
+/* Sockets time out, set shorter if you have mutiple rigs specified in anubis
+   but turned off / disabled and page refreshes take a long time */
+
+$socket_timeout = 3;
+?>
